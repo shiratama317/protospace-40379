9957bb3356dd30957b53f780c89e6a6e8165f69a
shiratama317 <shiratamax6x5@gmail.com>
2024-02-21 15:51:38 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,56 @@
+class PrototypesController < ApplicationController
+before_action :set_prototype, only: [:show, :edit, :update]
+before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
+  def index
+    @prototypes = Prototype.all.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+end
